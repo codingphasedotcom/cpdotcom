b5aeec04a28a505a4d3255016b7932ae02ce64b7
Joe Santos Garcia <joesantosgarcia@gmail.com>
2019-01-06 13:15:40 -0500

    Fixes

--- src/pages/courses/learn-php-7-and-laravel-master-course.js
+++ src/pages/courses/learn-php-7-and-laravel-master-course.js
@@ -158,7 +158,7 @@ class Page extends Component {
         <section id="course-highlight">
           <div className="section-content">
             <div className="container">
-              <h3>Modern Javascript</h3>
+              <h3>Modern PHP</h3>
               <div className="grid-container">
                 <div className="column">
                   <h1>Blade</h1>

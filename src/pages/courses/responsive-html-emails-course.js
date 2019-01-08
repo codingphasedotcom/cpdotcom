72ec310973a9c3e03d3dadb0670569c37ca0ace8
Joe Santos Garcia <joesantosgarcia@gmail.com>
2019-01-08 17:38:01 -0500

    added new course

--- src/pages/courses/responsive-html-emails-course.js
+++ src/pages/courses/responsive-html-emails-course.js
@@ -152,7 +152,7 @@ class Page extends Component {
         <section id="course-highlight">
           <div className="section-content">
             <div className="container">
-              <h3>Modern HTML Emails</h3>
+              <h3>Modern Development</h3>
               <div className="grid-container">
                 <div className="column">
                   <h1>Media Queries</h1>

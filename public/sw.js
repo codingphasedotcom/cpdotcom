c22e2581467280e26c76ff9c2bb39c03ed9e01e4
joe santos garcia <joesantosgarcia@gmail.com>
2019-10-21 11:58:35 -0400

    setup landing page

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 //Delete all caches and keep only one
-const cachNameToKeep = 'codingphase-10-202-19';
+const cachNameToKeep = 'codingphase-10-205-19';
 
 //Deletion should only occur at the activate event
 self.addEventListener('activate', event => {
@@ -21,6 +21,7 @@ self.addEventListener('activate', event => {
 });
 
 //Comment this whole area when working on development mode and then remove commnent when ready for production
+
 self.addEventListener('install', function(e) {
 	self.skipWaiting();
 	e.waitUntil(
